542a1a26e1911b582f7278c31bd0f0bbee8bc51c
fahadalamjoy <joy.fahadalamjoy@gmail.com>
2021-12-06 15:35:36 +0600

    my firtst branch

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import { Text, View } from 'react-native';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
+import { AppLoading } from 'expo'
+import * as Font from 'expo-font'
+import React, { useState } from 'react'
+import { Text, View } from 'react-native'
 
 const fetchFonts = () => {
   return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  });
-};
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  })
+}
 
 export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
+  const [fontLoaded, setFontLoaded] = useState(false)
 
   if (!fontLoaded) {
     return (
@@ -19,12 +19,13 @@ export default function App() {
         startAsync={fetchFonts}
         onFinish={() => setFontLoaded(true)}
       />
-    );
+    )
   }
 
   return (
     <View>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Creating a Branch</Text>
     </View>
-  );
+  )
 }
